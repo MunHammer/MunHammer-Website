MunHammer/MunHammer-Website

commit a056906cde147de499b45a68786aa3e7760a59a3

Added small assorted fixes - Changed replacePLaceholders to replacePlaceholders - Changed method of inserting HTML - Fixed font name - Removed redundant CSS

src/main.ts
@@ -7,7 +7,7 @@ MunHammer's website is distributed in the hope that it will be useful, but WITHO
 
 You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
 */
-function replacePLaceholders(): boolean {
+function replacePlaceholders(): boolean {
   // The heading
   const HEADING = `
     <h2 id="title">MunHammer.com</h2>
@@ -21,7 +21,7 @@ function replacePLaceholders(): boolean {
   `
   const headerElement = document.getElementById("header");
   if (headerElement) {
-    headerElement.innerHTML = HEADING + headerElement.innerHTML;
+    headerElement.insertAdjacentHTML("beforebegin", HEADING);
   } else {
     return false;
   }
@@ -55,9 +55,7 @@ function replacePLaceholders(): boolean {
   return true;
 }
 function main() {
-  if (!replacePLaceholders()) {
+  if (!replacePlaceholders())
     console.error("Couldn't find elements required")
-  }
-
 }
 main();

src/styles.css
@@ -9,7 +9,7 @@ You should have received a copy of the GNU General Public License along with Foo
 */
 :root {
   color-scheme: light dark;
-  font-family: Avenir, Monsterrat, Corbel, "URW Gothic", source-sans-pro, sans-serif;
+  font-family: Avenir, Montserrat, Corbel, "URW Gothic", source-sans-pro, sans-serif;
   --light: rgb(215 192 235);
   --dark: rgb(103 80 134);
 }
@@ -47,7 +47,6 @@ footer nav div {
 }
 
 footer nav div ul {
-  list-style-type: none;
   list-style: none;
   padding-left: 0;
 }

commit 36283b615da64d7e0c0c9143bd42185c68ba9bb4

Fixed bug where code would insert header at the wrong place Bug was introduced in commit a056906cde147de499b45a68786aa3e7760a59 a3

src/main.ts
@@ -21,7 +21,7 @@ function replacePlaceholders(): boolean {
   `
   const headerElement = document.getElementById("header");
   if (headerElement) {
-    headerElement.insertAdjacentHTML("beforebegin", HEADING);
+    headerElement.insertAdjacentHTML("afterbegin", HEADING);
   } else {
     return false;
   }